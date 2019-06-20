0f87901b68fc5b7efd0c36c917774d2b901d5e62
John Morley <41190821+johnmorleybjss@users.noreply.github.com>
2019-06-20 15:40:38 +0100

    MES-2591/2592: Update test and journal schemas to include fields for RSIS (#40)
    * Include examinerVisiting, candidate's dateOfBirth, gender and ethnicOriginCode, examiner's invididualId in the journal schema + test schema
    * Add a new captured field to test schema, accompaniment by interpreter
    * Bump to @dvsa/mes-journal-schema@1.0.7 and @dvsa/mes-test-schema@1.0.21

--- mes-test-schema/categories/B/index.d.ts
+++ mes-test-schema/categories/B/index.d.ts
@@ -6,6 +6,10 @@
  */
 
 /**
+ * The gender of an individual, limited to 'M' or 'F' as per TARS master data
+ */
+export type Gender = "M" | "F";
+/**
  * Code representing the result of the test
  */
 export type ActivityCode =
@@ -169,6 +173,10 @@ export interface Examiner {
    * The examiner's DSA staff number
    */
   staffNumber: string;
+  /**
+   * The individual ID of the examiner
+   */
+  individualId?: number;
 }
 /**
  * Details of the test centre
@@ -211,6 +219,10 @@ export interface TestSlotAttributes {
    * Whether this is an extended test
    */
   extendedTest: boolean;
+  /**
+   * Whether the examiner conducting the test slot is visiting a test centre that's not their home test centre
+   */
+  examinerVisiting?: boolean;
 }
 /**
  * Details of the candidate booked into the test slot
@@ -225,6 +237,11 @@ export interface Candidate {
    * The candidate's driver number, typically (though not always) 16 characters if UK, or 8 digits if NI
    */
   driverNumber?: string;
+  /**
+   * The candidate's date of birth, formatted as an ISO 8601 date (YYYY-MM-DD)
+   */
+  dateOfBirth?: string;
+  gender?: Gender;
   candidateAddress?: Address;
   /**
    * The candidate's primary telephone number, if any (and consent to leave voicemail has been given)
@@ -250,6 +267,10 @@ export interface Candidate {
    * The number of previous test attempts, if an ADI test
    */
   previousADITests?: number;
+  /**
+   * A number defining a candidate's ethnic origin, based on TARS master data
+   */
+  ethnicOriginCode?: number;
 }
 /**
  * Details of the individual's name
@@ -353,6 +374,10 @@ export interface Accompaniment {
    * Indicates whether a DVSA supervisor was present during the test
    */
   supervisor?: boolean;
+  /**
+   * Indicates whether an interpreter was present during the test
+   */
+  interpreter?: boolean;
   /**
    * Indicates whether another individual was present during the test
    */
