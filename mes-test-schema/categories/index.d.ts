39699a7a59beaef4d217cc4c996af2358a422c78
Ahmed Mobarak <ahmed.mobarak@hmcts.net>
2019-12-12 10:41:46 +0000

    MES-4028-Created the category C schema along with unique types and unions

--- mes-test-schema/categories/index.d.ts
+++ mes-test-schema/categories/index.d.ts
@@ -1,10 +1,13 @@
 import { PartialTestResultCatBSchema } from "./B/partial";
 import { PartialTestResultCatBESchema } from "./BE/partial";
+import { PartialTestResultCatCSchema } from "./C/partial";
 
 import { TestResultCommonSchema } from "./common/index";
 import { CatBUniqueTypes } from "./B";
 import { CatBEUniqueTypes } from "./BE";
+import { CatCUniqueTypes } from "./C";
 
 export type TestResultSchemasUnion =
   | CatBUniqueTypes.TestResult
-  | CatBEUniqueTypes.TestResult;
+  | CatBEUniqueTypes.TestResult
+  | CatCUniqueTypes.TestResult;
