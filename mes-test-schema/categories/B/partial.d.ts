df19e964f59dde0ddfc8aa858e443efd1b725530
Ahmed Mobarak <ahmed.mobarak@hmcts.net>
2019-12-16 10:17:59 +0000

    No additional properties for cat B vehicleDetails

--- mes-test-schema/categories/B/partial.d.ts
+++ mes-test-schema/categories/B/partial.d.ts
@@ -167,8 +167,4 @@ export interface QuestionResult {
 export interface VehicleDetails {
   schoolCar?: SchoolCar;
   dualControls?: DualControls;
-<<<<<<< HEAD
-=======
-  [k: string]: any;
->>>>>>> MES-4028-All category C schemas done
 }
