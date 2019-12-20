1531daffba0fac61e4e8c72cc1837d44bd43e2ae
ammarhaiderbjss <33695049+ammarhaiderbjss@users.noreply.github.com>
2019-12-20 14:20:54 +0000

    MES-4261: Category C+E schema (#116)
    * MES-4028-All category C schemas done
    
    * No additional properties for cat B vehicleDetails
    
    * udpates C1, CE and C1E to mirror B/BE file structure
    
    * bumps minor version
    
    * removes everything but C+E
    
    * adds uncouple recouple type to Cat C schema namespace

--- mes-test-schema/categories/index.d.ts
+++ mes-test-schema/categories/index.d.ts
@@ -2,8 +2,10 @@ import { TestResultCommonSchema } from "./common/index";
 import { CatBUniqueTypes } from "./B";
 import { CatBEUniqueTypes } from "./BE";
 import { CatCUniqueTypes } from "./C";
+import { CatCEUniqueTypes } from "./CE";
 
 export type TestResultSchemasUnion =
   | CatBUniqueTypes.TestResult
   | CatBEUniqueTypes.TestResult
-  | CatCUniqueTypes.TestResult;
+  | CatCUniqueTypes.TestResult
+  | CatCEUniqueTypes.TestResult;
