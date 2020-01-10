ff31ec95a3ac1f374a86e2170e7458b12ad3d455
Matt <mculliford.dvsa@icloud.com>
2020-01-10 09:10:11 +0000

    add AM1 category

--- mes-test-schema/generateSchemas.ts
+++ mes-test-schema/generateSchemas.ts
@@ -7,7 +7,7 @@ const fs = require('fs');
 const prettyJs = require('pretty-js');
 const json2ts = require('json-schema-to-typescript');
 
-const categories = ['B', 'BE', 'C', 'CE', 'C1', 'C1E'];
+const categories = ['B', 'BE', 'C', 'CE', 'C1', 'C1E', 'AM1'];
 
 // Json styling config
 const options = {
