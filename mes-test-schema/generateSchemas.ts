7f574ea617e8f1e905a5d7889f76842f1d368daf
Matt <mculliford.dvsa@icloud.com>
2020-01-13 13:06:42 +0000

    update schema generation script

--- mes-test-schema/generateSchemas.ts
+++ mes-test-schema/generateSchemas.ts
@@ -34,10 +34,6 @@ if (cliArgument === 'clean') {
 if (cliArgument === 'generate') {
   // Generate common typescript file
   generateTypescriptInterfaces(`./category-definitions/common/index.json`, `./categories/common/index.d.ts`);
-  //generate Cat A mod 1 typescript file (standalone)
-  generateTypescriptInterfaces(`./category-definitions/AM1/partial.json`, `./categories/AM1/index.d.ts`);
-  //copy Cat A mod 1 index.json file
-
   for (let category of categories) {
     const partialSchema = `./category-definitions/${category}/partial.json`;
     const combinedSchema = `./categories/${category}/index.json`;
@@ -50,6 +46,16 @@ if (cliArgument === 'generate') {
     // DO NOT REGENERATED THIS FILE. IT HAS BEEEN MODIFIED BY HAND
     // generateTypescriptInterfaces(combinedSchema);
   }
+  // generate Cat A mod 1 typescript file (standalone)
+  generateTypescriptInterfaces(`./category-definitions/AM1/index.json`, `./categories/AM1/index.d.ts`);
+  // copy Cat A mod 1 index.json file
+  fs.copyFile(`./category-definitions/AM1/index.json`, `./categories/AM1/index.json`, (err) => {
+    if(err) throw err;
+  });
+  // copy common index.json file across for consistency
+  fs.copyFile(`./category-definitions/common/index.json`, `./categories/common/index.json`, (err) => {
+    if(err) throw err;
+  });
   console.log('All categories have been generated');
 }
 
