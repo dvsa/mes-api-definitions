cf714c62121dbed2e221257f1b1f1181531007b7
Matt <mculliford.dvsa@icloud.com>
2020-01-13 15:38:31 +0000

    reference common types for candidate and journal

--- mes-test-schema/categories/AM1/index.d.ts
+++ mes-test-schema/categories/AM1/index.d.ts
@@ -1,25 +1,26 @@
 import { TestResultCatAM1Schema } from './partial';
 import { TestData as CatAM1TestData} from './partial';
 import { VehicleDetails as CatAM1VehicleDetails } from './partial';
-import { JournalData as CatAM1JournalData } from './partial';
-import { Candidate as CatAM1Candidate } from './partial';
+import { JournalData as CommonJournalData} from '../common';
+import { Candidate as CommonCandidate } from '../common';
 import { Circuit as CatAM1Circuit } from './partial';
 import { CategoryCode as CatAM1CategoryCode } from './partial';
 import { Avoidance as CatAM1Avoidance } from './partial';
 import { EmergencyStop as CatAM1EmergencyStop } from './partial'
 
 export declare namespace CatAMod1UniqueTypes {
+
   type TestResult = TestResultCatAM1Schema;
 
   type TestData = CatAM1TestData;
 
   type VehicleDetails = CatAM1VehicleDetails;
 
-  type JournalData = CatAM1JournalData;
+  type JournalData = CommonJournalData;
 
-  type Candidate = CatAM1Candidate;
+  type Candidate = CommonCandidate;
 
-  type Circuit = Circuit;
+  type Circuit = CatAM1Circuit;
 
   type CategoryCode = CatAM1CategoryCode;
 
