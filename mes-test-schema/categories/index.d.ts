6edfa6b0d345d4756a65f6d01edae7419e3d5cf8
Ahmed <a.mobarak92@gmail.com>
2020-02-04 11:18:19 +0000

    Adding cat Ds to TestResultSchemasUnion type (#133)

--- mes-test-schema/categories/index.d.ts
+++ mes-test-schema/categories/index.d.ts
@@ -5,6 +5,10 @@ import { CatCUniqueTypes } from './C';
 import { CatCEUniqueTypes } from './CE';
 import { CatC1UniqueTypes } from './C1';
 import { CatC1EUniqueTypes } from './C1E';
+import { CatDUniqueTypes } from './D';
+import { CatDEUniqueTypes } from './DE';
+import { CatD1UniqueTypes } from './D1';
+import { CatD1EUniqueTypes } from './D1E';
 import { TestResultCatAM1Schema } from './AM1';
 import { TestResultCatAM2Schema } from './AM2';
 
@@ -15,5 +19,9 @@ export type TestResultSchemasUnion =
   | CatCEUniqueTypes.TestResult
   | CatC1UniqueTypes.TestResult
   | CatC1EUniqueTypes.TestResult
+  | CatDUniqueTypes.TestResult
+  | CatDEUniqueTypes.TestResult
+  | CatD1UniqueTypes.TestResult
+  | CatD1EUniqueTypes.TestResult
   | TestResultCatAM1Schema
   | TestResultCatAM2Schema;
