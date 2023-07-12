a30fda1a14a76dffc87b8ff95d86b97c92094c77
Matthew Bell <mbell.dvsa@icloud.com>
2023-07-12 09:37:14 +0100

    Add make, model, testExpiryDate & previouslySearchedRegNumbers

--- mes-test-schema/generateSchemas.ts
+++ mes-test-schema/generateSchemas.ts
@@ -21,39 +21,39 @@ const standaloneCategories = ['AM1', 'AM2', 'CPC', 'ADI3'];
 
 // Json styling config
 const options = {
-  indent: "\t",
-  newline: "\r\n",
-  quoteProperties: "true"
+    indent: "\t",
+    newline: "\r\n",
+    quoteProperties: "true"
 };
 
 const cliArgument = process.argv[2];
 
 // Clears and cleans the generated schemas/typescripts
 if (cliArgument === 'clean') {
-  deleteFileIfExists(`./categories/common/index.d.ts`);
-  for (let category of categories) {
-    deleteFileIfExists(`./categories/${category}/index.json`);
-    deleteFileIfExists(`./categories/${category}/partial.d.ts`);
-    // DO NOT DELETE THIS FILE IT HAS BEEN MODIFIED BY HAND
-    // AND WILL NOT BE REGENERATED
-    // deleteFileIfExists(`./categories/${category}/index.d.ts`);
-  }
-  console.log('All categories have been cleaned');
+    deleteFileIfExists(`./categories/common/index.d.ts`);
+    for (let category of categories) {
+        deleteFileIfExists(`./categories/${category}/index.json`);
+        deleteFileIfExists(`./categories/${category}/partial.d.ts`);
+        // DO NOT DELETE THIS FILE IT HAS BEEN MODIFIED BY HAND
+        // AND WILL NOT BE REGENERATED
+        // deleteFileIfExists(`./categories/${category}/index.d.ts`);
+    }
+    console.log('All categories have been cleaned');
 }
 
 if (cliArgument === 'generate') {
     // Generate common typescript file
     generateTypescriptInterfaces(`./category-definitions/common/index.json`, `./categories/common/index.d.ts`);
     for (let category of categories) {
-        const partialSchema = `./category-definitions/${ category }/partial.json`;
-        const combinedSchema = `./categories/${ category }/index.json`;
+        const partialSchema = `./category-definitions/${category}/partial.json`;
+        const combinedSchema = `./categories/${category}/index.json`;
 
         // Generate combined schemas
         generateCombinedSchema(partialSchema, combinedSchema);
 
         // Generate typescript files
-        generateTypescriptInterfaces(partialSchema, `./categories/${ category }/partial.d.ts`);
-        // DO NOT REGENERATED THIS FILE. IT HAS BEEEN MODIFIED BY HAND
+        generateTypescriptInterfaces(partialSchema, `./categories/${category}/partial.d.ts`);
+        // DO NOT REGENERATE THIS FILE. IT HAS BEEN MODIFIED BY HAND
         // generateTypescriptInterfaces(combinedSchema);
     }
 
@@ -70,56 +70,59 @@ if (cliArgument === 'generate') {
 }
 
 function copyFile(source: string, destination: string) {
-  fs.copyFile(source, destination, (err) => {
-    if(err) throw err;
-  });
+    fs.copyFile(source, destination, (err: NodeJS.ErrnoException | null) => {
+        if (err) throw err;
+    });
 }
 
 function generateCombinedSchema(partialSchemaLocation: string, combinedSchemaLocation: string) {
-  const currentCategoryJson = require(partialSchemaLocation);
-  currentCategoryJson['title'] = currentCategoryJson['title'].replace('Partial ', '');
-  const combinedJsonObject = removeRefs(merge(common, currentCategoryJson));
-  let combinedJsonString = JSON.stringify(combinedJsonObject);
-  combinedJsonString = combinedJsonString.replace(/..\/common\/index.json/g, '');
-  fs.writeFileSync(combinedSchemaLocation, prettyJs(combinedJsonString, options));
+    const currentCategoryJson = require(partialSchemaLocation);
+    currentCategoryJson['title'] = currentCategoryJson['title'].replace('Partial ', '');
+    const combinedJsonObject = removeRefs(merge(common, currentCategoryJson));
+    let combinedJsonString = JSON.stringify(combinedJsonObject);
+    combinedJsonString = combinedJsonString.replace(/..\/common\/index.json/g, '');
+    fs.writeFileSync(combinedSchemaLocation, prettyJs(combinedJsonString, options));
 }
 
 function generateTypescriptInterfaces(schemaLocation: string, saveToLocation: string) {
-  json2ts.compileFromFile(schemaLocation, {
-    unreachableDefinitions: true
-  })
-    .then(ts => fs.writeFileSync(saveToLocation, ts))
+    json2ts.compileFromFile(schemaLocation, {
+        unreachableDefinitions: true
+    }).then((ts: string) => fs.writeFileSync(saveToLocation, ts))
 }
 
 function deleteFileIfExists(fileLocation: string) {
-  try {
-    fs.unlinkSync(fileLocation);
-  } catch (e) { }
+    try {
+        fs.unlinkSync(fileLocation);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 // Remove all $ref if they have siblings
 function removeRefs(obj: any): any {
-  const removeRefsFields = (obj: any) => {
-    Object.keys(obj).forEach((key) => {
-      const value = obj[key];
-      if (key === '$ref' && Object.keys(obj).length > 1) delete obj[key];
-      if (typeof value === 'object') removeRefsFields(value);
-    });
-    return obj;
-  };
-  return removeRefsFields(obj);
+    const removeRefsFields = (obj: any) => {
+        Object.keys(obj).forEach((key) => {
+            const value = obj[key];
+            if (key === '$ref' && Object.keys(obj).length > 1) delete obj[key];
+            if (typeof value === 'object') removeRefsFields(value);
+        });
+        return obj;
+    };
+    return removeRefsFields(obj);
 }
 
 function deReferenceJsonSchema(sourcePath: string, targetPath: string): void {
-  const originalSchema = require(sourcePath);
-  const baseFolder = sourcePath.substring(0, sourcePath.lastIndexOf("/"));
-  let deReferencedJsonSchema = derefSchema(originalSchema, {
-    baseFolder: baseFolder,
-  });
-  let deReferencedSchema = prettyJs(JSON.stringify(deReferencedJsonSchema), options);
-  fs.writeFile(targetPath, deReferencedSchema, 'utf8', (err) => {
-    if (err) {
-      console.error(`unable to write to file ${targetPath}`);
-    }
-  })
+    const originalSchema = require(sourcePath);
+    const baseFolder = sourcePath.substring(0, sourcePath.lastIndexOf("/"));
+    let deReferencedJsonSchema = derefSchema(originalSchema, {
+        baseFolder: baseFolder,
+    });
+
+    let deReferencedSchema = prettyJs(JSON.stringify(deReferencedJsonSchema), options);
+
+    fs.writeFile(targetPath, deReferencedSchema, 'utf8', (err: NodeJS.ErrnoException | null) => {
+        if (err) {
+            console.error(`unable to write to file ${targetPath}`);
+        }
+    })
 }
